59cc64fc7eab1c225faca4971a5f7105ee128478
anand <anandpromax@gmail.com>
2024-07-07 17:46:13 +0530

    chore: Update llm_utils.py to use langchain_experimental.agents

--- streamlit-chatgpt-demo-app/src/llm_utils.py
+++ streamlit-chatgpt-demo-app/src/llm_utils.py
@@ -7,19 +7,17 @@ import streamlit as st
 
 from langchain.chat_models import ChatOpenAI
 from langchain.agents import AgentType
-from langchain.agents import create_pandas_dataframe_agent
+from langchain_experimental.agents import create_pandas_dataframe_agent
 from langchain.schema.output_parser import OutputParserException
 
-if os.environ.get('OPENAI_API_KEY') is not None:
-    openai.api_key = os.environ['OPENAI_API_KEY']
+if os.environ.get("OPENAI_API_KEY") is not None:
+    openai.api_key = os.environ["OPENAI_API_KEY"]
 else:
     _ = load_dotenv(find_dotenv())  # read local .env file
-    openai.api_key = os.environ['OPENAI_API_KEY']
+    openai.api_key = os.environ["OPENAI_API_KEY"]
 
 
-def chat_api(
-    messages, model="gpt-4", temperature=0.0, max_tokens=256, top_p=0.5
-):
+def chat_api(messages, model="gpt-4", temperature=0.0, max_tokens=256, top_p=0.5):
     """
     The chat API endpoint of the ChatGPT
 
@@ -44,10 +42,7 @@ def chat_api(
             Return the code <code> in the following
             format ```python <code>```
         """
-        messages.append({
-            "role": "assistant",
-            "content": code_prompt
-        })
+        messages.append({"role": "assistant", "content": code_prompt})
 
     response = openai.ChatCompletion.create(
         model=model,
@@ -57,18 +52,17 @@ def chat_api(
         top_p=top_p,
         frequency_penalty=0,
         presence_penalty=0,
-        stop=None
+        stop=None,
     )
 
     if plot_flag:
-        code = extract_python_code(
-            response["choices"][0]["message"]["content"])
+        code = extract_python_code(response["choices"][0]["message"]["content"])
         if code is None:
             st.warning(
                 "Couldn't find data to plot in the chat. "
                 "Check if the number of tokens is too low for the data at hand. "
                 "I.e. if the generated code is cut off, this might be the case.",
-                icon="🚨"
+                icon="🚨",
             )
         else:
             code = code.replace("fig.show()", "")
@@ -80,7 +74,7 @@ def chat_api(
 
 
 def extract_python_code(text):
-    pattern = r'```python\s(.*?)```'
+    pattern = r"```python\s(.*?)```"
     matches = re.findall(pattern, text, re.DOTALL)
     if not matches:
         return None
@@ -101,10 +95,7 @@ def chat_with_data_api(df, model="gpt-4", temperature=0.0, max_tokens=256, top_p
             Return the code <code> in the following
             format ```python <code>```
         """
-        st.session_state.messages.append({
-            "role": "assistant",
-            "content": code_prompt
-        })
+        st.session_state.messages.append({"role": "assistant", "content": code_prompt})
         response = openai.ChatCompletion.create(
             model=model,
             messages=st.session_state.messages,
@@ -118,7 +109,7 @@ def chat_with_data_api(df, model="gpt-4", temperature=0.0, max_tokens=256, top_p
                 "Couldn't find data to plot in the chat. "
                 "Check if the number of tokens is too low for the data at hand. "
                 "I.e. if the generated code is cut off, this might be the case.",
-                icon="🚨"
+                icon="🚨",
             )
             return "Couldn't plot the data"
         else:
@@ -142,6 +133,7 @@ def chat_with_data_api(df, model="gpt-4", temperature=0.0, max_tokens=256, top_p
             return_intermediate_steps=True,
             agent_type=AgentType.OPENAI_FUNCTIONS,
             handle_parsing_errors=False,
+            allow_dangerous_code=True,
         )
 
         try:
@@ -154,6 +146,6 @@ def chat_with_data_api(df, model="gpt-4", temperature=0.0, max_tokens=256, top_p
             error_msg = """OutputParserException error occured in LangChain agent.
                 Refine your query."""
             return error_msg
-        except:  # noqa: E722
+        except:   # noqa: E722
             answer = "Unknown error occured in LangChain agent. Refine your query"
-            return error_msg
+            return answer
